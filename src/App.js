59f9ee399640b9ca5fc89f305e8a31a2e8713b4e
Chris Chow <christian.chow.en.yi@gmail.com>
2022-08-02 22:27:46 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import TypingView from "./components/TypingView/TypingView";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { data } from "./data";
+
+function App() {
+  // State
+  const [kbLayout, setKbLayout] = useState('QWERTY');
+  const [text, setText] = useState()
+
+  // Randomise texts
+  const getText = (textType) => {
+    const nTexts = data[textType].length;
+    const randomIdx = Math.floor(Math.random() * nTexts);
+    return data[textType][randomIdx];
+  }
+
+  // Initialise texts
+  useEffect(() => {
+    setText(getText('shortText'));
+  }, []);
+
+  const kbLayouts = [
+    {value: 'QWERTY', label: 'QWERTY'},
+    {value: 'Dvorak', label: 'Dvorak'},
+    {value: 'Colemak', label: 'Colemak'},
+    {value: 'QWYRFM', label: 'Carpalx QWYRFM'},
+    {value: 'QGMLWY', label: 'Carpalx QGMLWY'},
+  ];
+
+  return (
+    <Container className="main mb-5">
+      <div className="text-center">
+        <h1>UNQWERTIFIED</h1>
+      </div>
+      <div className="info-panel text-center mt-4">
+        <p>Randomise the text, choose a layout, click into the typing window, and start typing.</p>
+        <p>Hit <code>ESC</code> to restart the sequence.</p>
+      </div>
+      <Row className="justify-content-center mt-3">
+        <Col xs={6} xl={4}>
+          <Form.Select 
+            id="layout-select"
+            onChange={event => setKbLayout(event.target.value)}
+            value={kbLayout}
+          >
+            <option value="" disabled>Select a keyboard layout...</option>
+            {kbLayouts.map(item => {
+              return <option key={`select-${item.value}`} value={item.value}>{item.label}</option>
+            })}
+          </Form.Select>
+        </Col>
+      </Row>
+      <div className="justify-content-center d-flex mt-3">
+        <Button className="btn-green" onClick={() => setText(getText('shortText'))}>Short Text</Button>
+        <Button className="btn-purple" onClick={() => setText(getText('mediumText'))} style={{marginLeft: "10px"}}>Medium Text</Button>
+        <Button className="btn-red" onClick={() => setText(getText('longText'))} style={{marginLeft: "10px"}}>Long Text</Button>
+      </div>
+      {text && <TypingView layout={kbLayout} text={text} />}
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/TypingView/TypingView.js
@@ -0,0 +1,134 @@
+import { useEffect, useMemo, useState, useRef } from "react";
+import useTypingGame from "react-typing-game-hook";
+import Button from "react-bootstrap/Button";
+import translateText from "../../helpers/translateText";
+import './TypingView.css';
+
+export default function TypingView(props) {
+  // State
+  const letterElements = useRef(null);
+  const [focused, setFocused] = useState(false);
+
+  // Configure text
+  let textTransposed = translateText(props.text, props.layout);
+
+  // Create game object
+  const {
+    states: {
+      charsState,
+      length,
+      currIndex,
+      currChar,
+      correctChar,
+      phase,
+      startTime,
+      endTime
+    },
+    actions: { insertTyping, resetTyping, deleteTyping }
+  } = useTypingGame(textTransposed, {
+    skipCurrentWordOnSpace: false,
+    pauseOnError: true
+  });
+
+  // Handle keydowns
+  const handleKey = (key) => {
+    if (key === 'Escape') {
+      resetTyping();
+    } else if (key === 'Backspace') {
+      deleteTyping(false);
+    } else if (key.length === 1) {
+      insertTyping(key)
+    }
+  };
+
+  // Set cursor
+  const pos = useMemo(() => {
+    if (currIndex !== -1 && letterElements.current) {
+      let spanref = letterElements.current.children[currIndex];
+      let left = spanref.offsetLeft + spanref.offsetWidth - 2;
+      let top = spanref.offsetTop - 1;
+      return { left, top };
+    } else {
+      return {
+        left: 20,
+        top: 20
+      };
+    }
+  }, [currIndex]);
+
+  return (
+    <div className="mt-5">
+      {props.layout !== 'QWERTY' && phase !== 2 && 
+        <>
+          <div className="mt-5">
+            <h3>{props.layout} output:</h3>
+          </div>
+          <div className="typing-test mt-3">
+            <div className="text-display">
+              {phase !== 2 && props.text.split("").map((char, index) => {
+                let state = charsState[index];
+                let color = state === 0 ? "var(--light-grey-d5)" : state === 1 ? "var(--green)" : "var(--red)";
+                return (
+                  <span key={char + index}
+                    style={{ color }}
+                    className={currIndex + 1 === index ? "curr-letter" : ""}
+                  >
+                    {char}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      }
+      {phase !== 2 && <div className="true-text-header mt-5">
+        <h3>{props.layout === 'QWERTY' ? "Type:" : `${props.layout} using QWERTY:`}</h3>
+      </div>}
+      <div
+        className="typing-test mt-3"
+        onKeyDown={event => {
+          handleKey(event.key);
+          event.preventDefault();
+        }}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        tabIndex={0}
+      >
+        {phase !== 2 && <div className="main-text">
+          <div ref={letterElements} tabIndex={0}>
+            {textTransposed.split("").map((char, index) => {
+              let state = charsState[index];
+              let color = state === 0 ? "var(--light-grey-l3)" : state === 1 ? "#27DDCB" : "#FF5364";
+              return (
+                <span key={char + index}
+                  style={{ color }}
+                  className={currIndex + 1 === index ? "curr-letter" : ""}
+                >
+                  {char}
+                </span>
+              );
+            })}
+          </div>
+          {phase !== 2 && focused && <div
+            style={{
+              position: "absolute",
+              left: pos.left,
+              top: pos.top
+            }}
+            className="caret"
+          >
+            &nbsp;
+          </div>}
+        </div>}
+      </div>
+      {phase === 2 &&
+        <div className="text-center mt-5">
+          <h3>And you're done!</h3>
+          <p>How was the {props.layout} layout?</p>
+          <Button variant="outline-light" onClick={() => resetTyping()}>Restart</Button>
+        </div>
+      }
+    </div>
+
+  );
+}
